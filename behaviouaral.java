0a9ae2dd77fc202b142e5d11e393fd208535236f
DIVYA-GUDISE <61285249+DIVYA-GUDISE@users.noreply.github.com>
2020-07-26 23:36:53 +0530

    Create behaviouaral.java

---
+++ behaviouaral.java
@@ -0,0 +1,32 @@
+public class Chat {
+    public static void showMessage(User user, String message) {
+        System.out.println(new Date().toString() + "[" + user.getName() + "]: " + message);
+    }
+}
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }   
+
+    public void sendMessage(String message) {
+        Chat.showMessage(this, message);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        User david = new User("David");
+        User scott = new User("Scott");
+        
+        david.sendMessage("Hi Scott! How are you?");
+        scott.sendMessage("I'm great! Thanks for asking. How are you?");
+    }
+}
